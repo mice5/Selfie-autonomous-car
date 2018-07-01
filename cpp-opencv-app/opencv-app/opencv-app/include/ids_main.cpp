f47fefe95fe3a5a8b6d5dd31e8b37482c089fe13
Unknown <mice.co@hotmail.com>
2018-07-01 14:54:16 +0200

    IDS no crashes

--- cpp-opencv-app/opencv-app/opencv-app/include/ids_main.cpp
+++ cpp-opencv-app/opencv-app/opencv-app/include/ids_main.cpp
@@ -1,63 +1,92 @@
 #include <iostream>
 
 #include <include/ids.h>
+#include <pthread.h>
+
 
 IDS_PARAMETERS ids_c;
-HIDS hCa = 1;
 
-int main_not(void){
+pthread_cond_t algorithm_signal = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t algorithm_signal_mutex = PTHREAD_MUTEX_INITIALIZER;
+void *camera_thread(void *x){
+
+    while(true){
+
+        ids_c.frame_loop();
+
+    }
+    return NULL;
+}
+
+
+int main(void){
     struct timespec start, end;
     unsigned int licznik_czas = 0;
     float seconds = 0;
     float fps = 0;
-    cv::Mat ids_image(480,752,CV_8UC3);
-    cv::Mat frame_ids(480,752,CV_8UC3);
-    ids_c.initialize_camera(&hCa, ids_image);
-    ids_c.setting_auto_params(&hCa);
-    ids_c.change_params(&hCa);
+    cv::Mat frame(IDS_HEIGHT, IDS_WIDTH, CV_8UC3);
+    pthread_mutex_init(&algorithm_signal_mutex,NULL);
+    ids_c.initialize_camera();
+    ids_c.setting_auto_params();
+    ids_c.change_params();
 
     ids_c.create_trackbars();
-    cvNamedWindow("frame_ids",1);
     cvNamedWindow("fram",1);
+
+    pthread_t frame_thread;
+
+    int x = 0;
+    if (pthread_create(&frame_thread,NULL,camera_thread,&x)){
+        std::cout<<"Error creating thread"<<std::endl;
+    }
+    else{
+         std::cout << "Thread started" << std::endl;
+    }
 
     while(true)
     {
 
-        if(licznik_czas == 0)
-        {
-            clock_gettime(CLOCK_MONOTONIC, &start);
-        }
-        cv::namedWindow( "fram", WINDOW_AUTOSIZE );
-        HWND* window = cv::cvGetWindowHandle("fram");
-        ids_c.get_frame(&hCa,480,752,ids_image, window);
-//        ids_c.update_params(&hCa);
+//        ids_c.frame_loop();
+        pthread_cond_wait(&algorithm_signal, &algorithm_signal_mutex);
+        static int denom = 0;
+        if(++denom > 10){
+            denom = 0;
+            if(licznik_czas == 0)
+            {
+                clock_gettime(CLOCK_MONOTONIC, &start);
+            }
 
-//        cv::imshow("fram",ids_image);
+            pthread_mutex_lock(&ids_c.frame_mutex);
 
-        if(licznik_czas > 500)
-        {
-            licznik_czas = 0;
-            clock_gettime(CLOCK_MONOTONIC, &end);
-            seconds = (end.tv_sec - start.tv_sec);
-            fps  =  1 / (seconds / 500);
-//            std::cout <<"FPS: " << fps << std::endl;
-            float fps;
-			if (is_GetFramesPerSecond(hCa, &fps) == IS_SUCCESS)
-				std::cout << "FPS: " << fps << std:: endl;
-			std::cout << "BW: " << is_GetUsedBandwidth (hCa) << std:: endl;
+            ids_c.ids_frame.copyTo(frame);
+            pthread_mutex_unlock(&ids_c.frame_mutex);
+
+            char s[20];
+            sprintf(s, "FPS: %lf", ids_c.getFPS());
+            cv::putText(frame, s, cv::Point(IDS_WIDTH/2, IDS_HEIGHT-30), CV_FONT_HERSHEY_DUPLEX, 0.4, CV_RGB(0, 255, 0), 1.0);
+            cv::imshow("fram",frame);
+            if(licznik_czas > 50)
+            {
+                licznik_czas = 0;
+                clock_gettime(CLOCK_MONOTONIC, &end);
+                seconds = (end.tv_sec - start.tv_sec);
+//                fps  =  10 / (seconds / 50);
+                fps = ids_c.getFPS();
+                std::cout <<"center_pixel: " << frame.at<int>(IDS_WIDTH/2, IDS_HEIGHT/2) << std::endl;
+                std::cout <<"FPS: " << fps << std::endl;
+            }
+            else
+            {
+                licznik_czas++;
+            }
+
+            cv::waitKey(10);
         }
-        else
-        {
-            licznik_czas++;
-        }
-        cv::waitKey(1);
 
 
 
     }
-
-    is_FreeImageMem(hCa, ids_c.pMem, ids_c.memID);
-    is_ExitCamera(hCa);
+    ids_c.exit();
 
 
     return 0;
